6a6b90447635db31e23994fb000b18e591ed30af
ron-ait <rohit.nagose@aitglobalinc.com>
2023-12-21 18:27:04 +0530

    update python code

--- producer.py
+++ producer.py
@@ -1,38 +1,30 @@
 from faker import Faker
 from kafka import KafkaProducer
-import json
-import geojson
+import json 
 import time 
-import random
-
 
 producer = KafkaProducer(
     bootstrap_servers='localhost:29092',
     value_serializer= lambda x: json.dumps(x).encode('utf-8')
 )
 
-#def generate_random_location(): 
- #   latitude = random.uniform(-90,90)
- #   longitude =  random.uniform(-180,180)
-  #  return (latitude, longitude)
-
 
 fake = Faker()
 i =0
 while i < 100:
     message= {
-        "name": fake.name(),
-        "latitude": random.uniform(-90,90), 
-        "longitude": random.uniform(-180,180),
-        "phone": fake.phone_number()
+        'name':fake.name(),
+        'city':fake.city(),
+        'phone':fake.phone_number()
         }
-    kafka_key = 'message_update'.encode('utf-8')
+    key = 'message_update'.encode('utf-8')
 
 
     producer.send(
         topic='new-updates',
-        key=kafka_key,
+        key=key,
         value=message
     )
+    time.sleep(1)
     i=i+1
 
